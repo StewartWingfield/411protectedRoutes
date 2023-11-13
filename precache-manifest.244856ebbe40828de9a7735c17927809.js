f6592847ae75ffdeac2c105f8680df21e1dc521d
Stewart Wingfield <sww915@gmail.com>
2023-11-13 13:56:19 -0500

    Updates

---
+++ precache-manifest.244856ebbe40828de9a7735c17927809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064d9be836e7d642aef87cc0e5769aca",
+    "url": "/411protectedRoutes/index.html"
+  },
+  {
+    "revision": "6017c1a75684164f4eb1",
+    "url": "/411protectedRoutes/static/css/main.909db9c4.chunk.css"
+  },
+  {
+    "revision": "2d552553f6f53ea48c04",
+    "url": "/411protectedRoutes/static/js/2.b61baf83.chunk.js"
+  },
+  {
+    "revision": "6017c1a75684164f4eb1",
+    "url": "/411protectedRoutes/static/js/main.bfb1f0ac.chunk.js"
+  },
+  {
+    "revision": "9163cba63a8b805b8480",
+    "url": "/411protectedRoutes/static/js/runtime~main.3e0c5a79.js"
+  }
+]);
